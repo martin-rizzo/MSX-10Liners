1a23bfafa6362a4df88d53df2979ee56184c8bef
Martin Rizzo <martinrizzo@gmail.com>
2021-02-10 12:30:15 -0300

    Map more keys from Mac to MSX in the "/EXTRA/TEMPLATE/xrunemu.sh" script

--- EXTRA/TEMPLATE/xrunemu.sh
+++ EXTRA/TEMPLATE/xrunemu.sh
@@ -24,16 +24,54 @@ set blur 0;
 set accuracy screen;
 '
 
-# MAP F6 TO THE MSX "INSERT" KEY
-MAP_F6_TO_INSERT='
-bind F6         "keymatrixdown 8 0x04";
-bind F6,release "keymatrixup   8 0x04";
+# MAP CTRL+TAB TO THE MSX "TAB" KEY
+MAP_CTRLTAB_TO_TAB='
+bind CTRL+TAB         "keymatrixdown 7 0x08";
+bind CTRL+TAB,release "keymatrixup   7 0x08";
 '
 
-# RUN OPENMSX EMULATOR
-openmsx -machine "$MSX_TYPE" -diska "$DISK_A" -command "$HIGH_FRMERATE" -command "$SCALE_X2"  -command "$MAP_F6_TO_INSERT"
+# MAP TAB TO THE MSX "CAPS" KEY
+MAP_TAB_TO_CAPS='
+bind TAB         "keymatrixdown 6 0x08";
+bind TAB,release "keymatrixup   6 0x08";
+'
 
+# MAP PAGEUP TO THE MSX "INSERT" KEY
+MAP_PAGEUP_TO_INSERT='
+bind PAGEUP         "keymatrixdown 8 0x04";
+bind PAGEUP,release "keymatrixup   8 0x04";
+'
 
+# MAP END TO THE MSX "STOP" KEY
+MAP_END_TO_STOP='
+bind END         "keymatrixdown 7 0x10";
+bind END,release "keymatrixup   7 0x10";
+'
+
+# MAP CTRL+END TO THE MSX "CTRL+STOP" KEY COMBINATION
+MAP_CTRLEND_TO_CTRLSTOP='
+bind CTRL+END         "keymatrixdown 6 0x02 ; keymatrixdown 7 0x10";
+bind CTRL+END,release "keymatrixup   7 0x10 ; keymatrixup   6 0x02";
+'
+
+# MAP PAGEDOWN TO THE MSX "SELECT" KEY
+MAP_PAGEDOWN_TO_SELECT='
+bind PAGEDOWN         "keymatrixdown 7 0x40";
+bind PAGEDOWN,release "keymatrixup   7 0x40";
+'
+
+# == RUN OPENMSX EMULATOR ==
+COMMANDS="
+  $HIGH_FRAMERATE
+  $SCALE_X2
+  $MAP_CTRLTAB_TO_TAB
+  $MAP_TAB_TO_CAPS
+  $MAP_PAGEUP_TO_INSERT
+  $MAP_END_TO_STOP
+  $MAP_CTRLEND_TO_CTRLSTOP
+  $MAP_PAGEDOWN_TO_SELECT
+"
+openmsx -machine "$MSX_TYPE" -diska "$DISK_A" -command "$COMMANDS"
 
 
 
